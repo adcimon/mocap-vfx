ecd2772a980758452d8432ba898399136625436e
adcimon <37084114+adcimon@users.noreply.github.com>
2020-12-25 18:27:44 +0100

    Removed unused assets

--- Assets/Scripts/VNectBarracudaRunner.cs
+++ Assets/Scripts/VNectBarracudaRunner.cs
@@ -1,7 +1,6 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using Unity.Barracuda;
 
 /// <summary>
